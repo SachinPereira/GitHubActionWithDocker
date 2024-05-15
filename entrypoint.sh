3363380802b6dadbb699daf9ef6e108b17d7dacd
SachinPereira <56588858+SachinPereira@users.noreply.github.com>
2024-05-15 13:27:19 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-detect --blackduck.url="${BLACKDUCK_URL}" \
+detect9 --blackduck.url="${BLACKDUCK_URL}" \
        --blackduck.api.token="${BLACKDUCK_API_TOKEN}" \
        "$@"
