c8ff9a99423fd50f5b74d95e26b3b5d79140a61e
SachinPereira <56588858+SachinPereira@users.noreply.github.com>
2024-05-15 13:46:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # Run Synopsys Detect with your desired options
+chmod +X /synopsys-detect/detect9.sh
 /synopsys-detect/detect9.sh --blackduck.url="${BLACKDUCK_URL}" --blackduck.api.token="${BLACKDUCK_API_TOKEN}"
