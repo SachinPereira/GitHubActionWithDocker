a72cc07329598b0e3100581bf5e5073797101fe9
SachinPereira <56588858+SachinPereira@users.noreply.github.com>
2024-05-15 11:46:26 +0530

    Update entrypoint.sh

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 
 # Run Synopsys Detect
-java -jar synopsys-detect-${DETECT_VERSION}.jar --blackduck.url="$BLACKDUCK_URL" --blackduck.api.token="$BLACKDUCK_API_TOKEN" "$@"
+#java -jar synopsys-detect-${DETECT_VERSION}.jar --blackduck.url="$BLACKDUCK_URL" --blackduck.api.token="$BLACKDUCK_API_TOKEN" "$@"
